f2f541b69ec3c74772a9afa2b15832a24fbfe529
gouravchahar13 <1bi22cd014@bit-bangalore.edu.in>
2023-12-20 19:08:07 +0530

    updated

--- BIT_TECH_HUB/recipe_form/settings.py
+++ BIT_TECH_HUB/recipe_form/settings.py
@@ -140,8 +140,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_HOST="smtp.gmail.com"
-EMAIL_HOST_USER= "YOUR_EMAIL_HERE"
-EMAIL_HOST_PASSWORD= "YOUR_APP_PASSWORD_GMAIL"
+EMAIL_HOST_USER= "tutorialjaat13@gmail.com"
+EMAIL_HOST_PASSWORD= "jvsemvcbvdtznxux"
 EMAIL_PORT= 465
 # EMAIL_USE_TLS= True
 EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
